444b309e67b298495f7a0b2b99e64674a5ee6c83
Laertes Moustakas <lmoustak@gmail.com>
2019-03-06 11:21:34 +0200

    Added and changed a few features.

--- README.md
+++ README.md
@@ -2,13 +2,49 @@
 (WINDOWS ONLY) A TortoiseSVN menu extension for Brackets.
 
 ## Features
-Adds TortoiseSVN commands (commit, checkout, update, revert, log) in File menu and directory tree context menus.
+Adds TortoiseSVN commands like commit, switch, update, revert, log, diff, blame in File menu, directory tree and working set context menus.
 
 ## Prerequisites
 TortoiseSVN must be installed. Supports Windows only.
 
+## Using the extension
+Upon installing this extension, make the path to TortoiseProc.exe is **C:\Program Files\TortoiseSVN\bin\TortoiseProc.exe**.
+If not, you'll have to set the path in `brackets-tortoise-svn.tortoiseProcPath`'s value in brackets.json, like so:
+```json
+"brackets-tortoise-svn.tortoiseProcPath": "C:/path/to/TortoiseProc.exe"
+```
+You can also modify whether TortoiseSVN will close after execution for every command by changing the `brackets-tortoise-svn.closeOnEnd` values.
+```json
+// Defines when should the dialog box automatically close after the command executes: 0 - never; 1 - close if no errors; 2 - close if no errors and conflicts; 3 - close if no errors, conflicts and merges
+"brackets-tortoise-svn.closeOnEnd": {
+    // Default: 3
+    "commit": 3,
+
+    // Default: 0
+    "log": 0,
+
+    // Default: 0
+    "revert": 0,
+
+    // Default: 0
+    "switch": 0,
+
+    // Default: 0
+    "update": 0
+}
+```
+
+### File menu
+The File menu contains the commit, switch, update, revert, and log commands for the current project.
+![File menu](/images/fileMenu.png)
+
+### Project context menu
+The Project context menu (which appears after right-clicking on a file or folder in the project file tree), contains the aforementioned commands for the specific folder or file.
+![Project context menu](/images/projectContextMenu.png)
+
+### Working Files context menu
+The Working Files context menu (appearing when an open file is right-clicked), contains the diff and blame commands for that specific file.
+![Working Files context menu](/images/workingFilesContextMenu.png)
+
 ## License
 Published under the MIT license. See [LICENSE.md](LICENSE.md) for more details.
-
-## Notes
-This is my first repository on GitHub, and first Brackets extension (and first extension overall). So it may be a little rough around the edges.
--- main.js
+++ main.js
@@ -11,6 +11,7 @@ define(function (require, exports, module) {
         ExtensionUtils      = brackets.getModule("utils/ExtensionUtils"),
         NodeDomain          = brackets.getModule("utils/NodeDomain"),
         ProjectManager      = brackets.getModule("project/ProjectManager"),
+        MainViewManager     = brackets.getModule("view/MainViewManager"),
         PreferencesManager  = brackets.getModule("preferences/PreferencesManager"),
         prefs               = PreferencesManager.getExtensionPrefs("brackets-tortoise-svn");
     
@@ -19,30 +20,49 @@ define(function (require, exports, module) {
         values: ["C:/Program Files/TortoiseSVN/bin/TortoiseProc.exe", "C:/Program Files (x86)/TortoiseSVN/bin/TortoiseProc.exe"]
     });
     
-    prefs.definePreference("closeOnEnd", "object", {commit: 0, checkout: 0, update: 2, revert: 0, log: 0}, {
+    prefs.definePreference("closeOnEnd", "object", {
+        "commit": 3, 
+        "switch": 0, 
+        "update": 0, 
+        "revert": 0, 
+        "log": 0, 
+        "diff": 0, 
+        "blame": 0
+    }, 
+    {
         description: "Defines when should the dialog box automatically close after the command executes: 0 - never; 1 - close if no errors; 2 - close if no errors and conflicts; 3 - close if no errors, conflicts and merges",
         keys: {
-            commit: {
+            "commit": {
+                type: "number",
+                values: [0, 1, 2, 3],
+                initial: 3
+            },
+            "switch": {
                 type: "number",
                 values: [0, 1, 2, 3],
                 initial: 0
             },
-            checkout: {
+            "update": {
                 type: "number",
                 values: [0, 1, 2, 3],
                 initial: 0
             },
-            update: {
-                type: "number",
-                values: [0, 1, 2, 3],
-                initial: 2
-            },
-            revert: {
+            "revert": {
                 type: "number",
                 values: [0, 1, 2, 3],
                 initial: 0
             },
-            log: {
+            "log": {
+                type: "number",
+                values: [0, 1, 2, 3],
+                initial: 0
+            },
+            "diff": {
+                type: "number",
+                values: [0, 1, 2, 3],
+                initial: 0
+            },
+            "blame": {
                 type: "number",
                 values: [0, 1, 2, 3],
                 initial: 0
@@ -52,7 +72,7 @@ define(function (require, exports, module) {
 
     
     // Function to run when the menu item is clicked
-    function runTortoiseSVNCommand(command) {
+    function runTortoiseSVNCommand(command, path) {
         if (command === undefined) {
             window.alert("TortoiseSVN command is undefined");
             return;
@@ -61,7 +81,7 @@ define(function (require, exports, module) {
             return;
         }
         
-        var path = ProjectManager.getFileTreeContext() || ProjectManager.getProjectRoot();
+        path = path || ProjectManager.getProjectRoot();
         var execCommand = '"' + prefs.get("tortoiseProcPath") + '" /command:' + command + ' /closeonend:' + prefs.get("closeOnEnd")[command] + ' /path:"' + path.fullPath + '"';
         
         var shellCommandDomain = new NodeDomain("shellCommand", ExtensionUtils.getModulePath(module, "node/ShellCommandDomain"));
@@ -69,52 +89,62 @@ define(function (require, exports, module) {
     }
     
     function runTortoiseSVNCommit() {
-        runTortoiseSVNCommand("commit");
+        runTortoiseSVNCommand("commit", ProjectManager.getFileTreeContext());
     }
-    function runTortoiseSVNCheckout() {
-        runTortoiseSVNCommand("checkout");
+    function runTortoiseSVNSwitch() {
+        runTortoiseSVNCommand("switch", ProjectManager.getFileTreeContext());
     }
     function runTortoiseSVNUpdate() {
-        runTortoiseSVNCommand("update");
+        runTortoiseSVNCommand("update", ProjectManager.getFileTreeContext());
     }
     function runTortoiseSVNRevert() {
-        runTortoiseSVNCommand("revert");
+        runTortoiseSVNCommand("revert", ProjectManager.getFileTreeContext());
     }
     function runTortoiseSVNLog() {
-        runTortoiseSVNCommand("log");
+        runTortoiseSVNCommand("log", ProjectManager.getFileTreeContext());
+    }
+    function runTortoiseSVNDiff() {
+        runTortoiseSVNCommand("diff", MainViewManager.getCurrentlyViewedFile());
+    }
+    function runTortoiseSVNBlame() {
+        runTortoiseSVNCommand("blame", MainViewManager.getCurrentlyViewedFile());
     }
     
     
     // First, register a command - a UI-less object associating an id to a handler
     var SVN_PROJECT_COMMIT  = "brackets-tortoise-svn.project.commit",   // package-style naming to avoid collisions
-        SVN_PROJECT_CHECKOUT= "brackets-tortoise-svn.project.checkout",
+        SVN_PROJECT_SWITCH  = "brackets-tortoise-svn.project.switch",
         SVN_PROJECT_UPDATE  = "brackets-tortoise-svn.project.update",
         SVN_PROJECT_REVERT  = "brackets-tortoise-svn.project.revert",
         SVN_PROJECT_LOG     = "brackets-tortoise-svn.project.log",
         
         SVN_CONTEXT_COMMIT  = "brackets-tortoise-svn.context.commit",
-        SVN_CONTEXT_CHECKOUT= "brackets-tortoise-svn.context.checkout",
+        SVN_CONTEXT_SWITCH  = "brackets-tortoise-svn.context.switch",
         SVN_CONTEXT_UPDATE  = "brackets-tortoise-svn.context.update",
         SVN_CONTEXT_REVERT  = "brackets-tortoise-svn.context.revert",
-        SVN_CONTEXT_LOG     = "brackets-tortoise-svn.context.log";
+        SVN_CONTEXT_LOG     = "brackets-tortoise-svn.context.log",
+        SVN_CONTEXT_DIFF    = "brackets-tortoise-svn.context.diff",
+        SVN_CONTEXT_BLAME   = "brackets-tortoise-svn.context.blame";
     
     CommandManager.register("Tortoise SVN Commit Project", SVN_PROJECT_COMMIT, runTortoiseSVNCommit);
-    CommandManager.register("Tortoise SVN Checkout Project", SVN_PROJECT_CHECKOUT, runTortoiseSVNCheckout);
+    CommandManager.register("Tortoise SVN Switch Project", SVN_PROJECT_SWITCH, runTortoiseSVNSwitch);
     CommandManager.register("Tortoise SVN Update Project", SVN_PROJECT_UPDATE, runTortoiseSVNUpdate);
     CommandManager.register("Tortoise SVN Revert Project", SVN_PROJECT_REVERT, runTortoiseSVNRevert);
     CommandManager.register("Tortoise SVN Log Project", SVN_PROJECT_LOG, runTortoiseSVNLog);
     
     CommandManager.register("Tortoise SVN Commit", SVN_CONTEXT_COMMIT, runTortoiseSVNCommit);
-    CommandManager.register("Tortoise SVN Checkout", SVN_CONTEXT_CHECKOUT, runTortoiseSVNCheckout);
+    CommandManager.register("Tortoise SVN Switch", SVN_CONTEXT_SWITCH, runTortoiseSVNSwitch);
     CommandManager.register("Tortoise SVN Update", SVN_CONTEXT_UPDATE, runTortoiseSVNUpdate);
     CommandManager.register("Tortoise SVN Revert", SVN_CONTEXT_REVERT, runTortoiseSVNRevert);
     CommandManager.register("Tortoise SVN Log", SVN_CONTEXT_LOG, runTortoiseSVNLog);
+    CommandManager.register("Tortoise SVN Diff", SVN_CONTEXT_DIFF, runTortoiseSVNDiff);
+    CommandManager.register("Tortoise SVN Blame", SVN_CONTEXT_BLAME, runTortoiseSVNBlame);
 
     // Then create a menu item bound to the command
     // The label of the menu item is the name we gave the command (see above)
     var contextMenu = Menus.getContextMenu(Menus.ContextMenuIds.PROJECT_MENU);
     contextMenu.addMenuItem(SVN_CONTEXT_COMMIT);
-    contextMenu.addMenuItem(SVN_CONTEXT_CHECKOUT);
+    contextMenu.addMenuItem(SVN_CONTEXT_SWITCH);
     contextMenu.addMenuItem(SVN_CONTEXT_UPDATE);
     contextMenu.addMenuItem(SVN_CONTEXT_REVERT);
     contextMenu.addMenuItem(SVN_CONTEXT_LOG);
@@ -122,11 +152,16 @@ define(function (require, exports, module) {
     
     var fileMenu = Menus.getMenu(Menus.AppMenuBar.FILE_MENU);
     fileMenu.addMenuItem(SVN_PROJECT_COMMIT, null, Menus.LAST_IN_SECTION, Menus.MenuSection.FILE_SAVE_COMMANDS);
-    fileMenu.addMenuItem(SVN_PROJECT_CHECKOUT, null, Menus.LAST_IN_SECTION, Menus.MenuSection.FILE_SAVE_COMMANDS);
+    fileMenu.addMenuItem(SVN_PROJECT_SWITCH, null, Menus.LAST_IN_SECTION, Menus.MenuSection.FILE_SAVE_COMMANDS);
     fileMenu.addMenuItem(SVN_PROJECT_UPDATE, null, Menus.LAST_IN_SECTION, Menus.MenuSection.FILE_SAVE_COMMANDS);
     fileMenu.addMenuItem(SVN_PROJECT_REVERT, null, Menus.LAST_IN_SECTION, Menus.MenuSection.FILE_SAVE_COMMANDS);
     fileMenu.addMenuItem(SVN_PROJECT_LOG, null, Menus.LAST_IN_SECTION, Menus.MenuSection.FILE_SAVE_COMMANDS);
     fileMenu.addMenuDivider(Menus.BEFORE, SVN_PROJECT_COMMIT);
+    
+    var workingSetContextMenu = Menus.getContextMenu(Menus.ContextMenuIds.WORKING_SET_CONTEXT_MENU);
+    workingSetContextMenu.addMenuItem(SVN_CONTEXT_DIFF);
+    workingSetContextMenu.addMenuItem(SVN_CONTEXT_BLAME);
+    workingSetContextMenu.addMenuDivider(Menus.BEFORE, SVN_CONTEXT_DIFF);
     
     // We could also add a key binding at the same time:
     //menu.addMenuItem(MY_COMMAND_ID, "Ctrl-Alt-W");
